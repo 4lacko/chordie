64609fff077fae588cdabca78640478329fb317e
Flacko <f@ak68a.co>
2023-05-20 12:19:53 -0400

    Initial Commit

---
+++ Chordie/ChordieApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChordieApp.swift
+//  Chordie
+//
+//  Created by Ayo Moreira on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ChordieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
