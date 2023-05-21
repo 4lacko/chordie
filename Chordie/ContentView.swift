e3e172bf3ea91dc7288aa60a6e72cb3af6e3789a
Flacko <f@ak68a.co>
2023-05-20 21:22:40 -0400

    add button

--- Chordie/ContentView.swift
+++ Chordie/ContentView.swift
@@ -7,20 +7,49 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+var iq = intervalQuestions
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+var getQuestion = iq["C"]?.keys.randomElement()
+var getAnswer = iq["C"]?["D is the 2 of"]! // access answer
+
+
+struct ContentView: View {
+    @State private var showAnswer = false;
+    
+    
+    struct FlashCard: View {
+        var body: some View {
+            Text(getQuestion!)
+                .font(.title)
+        }
+    }
+    
+    var body: some View {
+        Button {
+            checkAnswer()
+        } label: {
+            FlashCard()
+        }
+        .frame(width: 200, height: 200)
+        .foregroundColor(.black)
+        .background(Color.mint)
+        .alert("Answer", isPresented: $showAnswer) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Test")
+        }
+        
+    }
+    
+    func checkAnswer() {
+        showAnswer = true
+    }
+    
+    func askQuestion() {}
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
